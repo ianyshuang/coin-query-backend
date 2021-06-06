ddc8eac0a81e88ccf7738f977c27ba5b36cc7d40
ianyshuang <ianyshuang@gmail.com>
2021-06-06 17:38:55 +0800

    fix: fix env variable mis-spelling

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ require('dotenv').config()
 const dynamoClient = new AWS.DynamoDB.DocumentClient({
   apiVersion: '2012-08-10',
   region: 'ap-northeast-1',
-  accessKeyId: process.env.DYNAMO_KEY,
-  secretAccessKey: process.env.DYNAMO_SECRET
+  accessKeyId: process.env.DYNAMO_FULL_ACCESS_KEY,
+  secretAccessKey: process.env.DYNAMO_FULL_ACCESS_SECRET
 })
 
 global.dynamoClient = dynamoClient
