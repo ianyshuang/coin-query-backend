871ccd24c129f6735903a7a8676a8e38f7682f6b
ianyshuang <ianyshuang@gmail.com>
2021-06-06 17:39:19 +0800

    feat: change "getCoinMarket()" structure due to change of table structure

--- controller/coin.js
+++ controller/coin.js
@@ -1,4 +1,15 @@
+const getTableLatestTime = async (table) => {
+  const { dynamoClient } = global
+  const coinUpdateTime = await dynamoClient.get({
+    TableName: 'CoinUpdateTime',
+    Key: { table: table }
+  }).promise()
+  
+  const timeSeries = coinUpdateTime.Item.datetime
+  const latestTime = timeSeries[timeSeries.length - 1]
 
+  return latestTime
+}
 
 module.exports = {
   getCoinMarket: async (req, res, next) => {
@@ -10,24 +21,16 @@ module.exports = {
       return res.status(400).send()
     }
 
-    let interval = 1000 * 60 * 5
-    let date = new Date()
-    let nearest = new Date(Math.round(date.getTime() / interval) * interval).getTime()
-    let result
-    while (true) {
-      result = await dynamoClient.get({
-        TableName: 'CoinMarket',
-        Key: {
-          datetime: nearest,
-          page: page
-        }
-      }).promise()
-      if (Object.keys(result).length === 0) {
-        nearest -= interval;
-      } else {
-        break
+    const tableName = 'CoinMarket'
+    const latestTime = await getTableLatestTime(tableName)
+    const pageData = await dynamoClient.get({
+      TableName: tableName,
+      Key: {
+        datetime: latestTime,
+        page: page
       }
-    }
-    return res.status(200).json(result)
+    }).promise()
+
+    return res.status(200).json(pageData)
   }
 }
